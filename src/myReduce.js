1b8bae992342153968edb82e2468ed59777bad8f
AristideI <a.isingizwe@alustudent.com>
2023-10-25 10:40:35 +0200

    updates

--- src/myReduce.js
+++ src/myReduce.js
@@ -1,4 +1,10 @@
-function reduce(arr, i = 0) {
-  if (i === arr.length) return 0;
-  return arr[i] + reduce(arr, i + 1);
+function reduce(arr, callBack, acc, index = 0) {
+  if (i === arr.length) return acc;
+  if (!acc && index === 0) {
+    return reduce(arr, callBack, arr[index], i + 1);
+  }
+  let value = callBack(acc, arr[i], i, arr);
+  return reduce(arr, callBack, value, index + 1);
 }
+
+console.log(reduce([1, 2, 3], (a, b) => a + b));
